901739ccb306ddc780af4ac95ac40b25cb68beb5
Kokila232425 <94901611+Kokila232425@users.noreply.github.com>
2025-01-16 19:20:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def webhook():
         return jsonify({"message": "Invalid request method"}), 405
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('ssl/server.crt', 'ssl/private.key'))
